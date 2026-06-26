5a5fd8008e28f00d01a4aa8f6257599681f2affa
dhruvnda1234 <dhruvnda1234@gmail.com>
2026-06-26 16:51:07 +0530

    Question10.c

---
+++ Question10.c
@@ -0,0 +1,11 @@
+//Find the size of different datatypes (int,float,char,double) using sizeof.
+#include<stdio.h>
+int main()
+{
+    printf("\nInteger size = %d",sizeof(int));
+    printf("\nFloat size = %d",sizeof(float));
+    printf("\nChar size = %d",sizeof(char));
+    printf("\nLong size = %d",sizeof(long));
+    printf("\nDouble size = %d",sizeof(double));
+    return 0;
+}
