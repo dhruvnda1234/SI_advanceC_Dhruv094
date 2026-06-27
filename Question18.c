22dd202c2d7b9298cd4dbb8335977c6e2e3a931f
dhruvnda1234 <dhruvnda1234@gmail.com>
2026-06-27 06:43:27 +0530

    Question18.c

---
+++ Question18.c
@@ -0,0 +1,21 @@
+//Check if a character is an alphabet, digit, or special character
+#include <stdio.h>
+
+int main() {
+    char ch;
+    printf("\nEnter any character: ");
+    scanf("%c", &ch);
+    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+    {
+        printf("'%c' is an alphabet.\n", ch);
+    }
+    else if (ch >= '0' && ch <= '9')
+    {
+        printf("'%c' is a digit.\n", ch);
+    }
+    else
+    {
+        printf("'%c' is a special character.\n", ch);
+    }
+    return 0;
+}
