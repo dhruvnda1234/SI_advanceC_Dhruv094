1dfc06e6ab9f14ba62e94cc844e2995faa9a4fbb
dhruvnda1234 <dhruvnda1234@gmail.com>
2026-06-28 18:14:37 +0530

    Question64.c

---
+++ Question64.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    char str[] = "dhruv yadav";
+    int freq[256] = {0};
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        freq[(unsigned char)str[i]]++;
+    }
+
+    for (int i = 0; i < 256; i++) {
+        if (freq[i] > 0) {
+            printf("'%c' : %d\n", i, freq[i]);
+        }
+    }
+    return 0;
+}
