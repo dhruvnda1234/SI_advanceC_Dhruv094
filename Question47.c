075f7e10a1cffe68d15ed6d7f138ff2c1f4444dc
dhruvnda1234 <dhruvnda1234@gmail.com>
2026-06-28 17:21:24 +0530

    Question47.c

---
+++ Question47.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11, 90, 34};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        int temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
