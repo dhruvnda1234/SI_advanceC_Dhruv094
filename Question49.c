c20846743f8f8977b93a7680480b8829ce21944d
dhruvnda1234 <dhruvnda1234@gmail.com>
2026-06-28 17:23:23 +0530

    Question49.c

---
+++ Question49.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int mat1[2][3] = {{1, 2, 3}, {4, 5, 6}};
+    int mat2[3][2] = {{7, 8}, {9, 10}, {11, 12}};
+    int result[2][2] = {{0, 0}, {0, 0}};
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            for (int k = 0; k < 3; k++) {
+                result[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
