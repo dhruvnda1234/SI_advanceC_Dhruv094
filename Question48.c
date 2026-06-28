6cd379e40c3aece8da3443f35222e48bc8731075
dhruvnda1234 <dhruvnda1234@gmail.com>
2026-06-28 17:22:39 +0530

    Question48.c

---
+++ Question48.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int mat1[2][2] = {{1, 2}, {3, 4}};
+    int mat2[2][2] = {{5, 6}, {7, 8}};
+    int sum[2][2];
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            sum[i][j] = mat1[i][j] + mat2[i][j];
+        }
+    }
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
